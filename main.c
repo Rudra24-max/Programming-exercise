b694c759749f3fe77168a35117bee0b967891425
Rudra24-max <rudra.nile@indiraicem.ac.in>
2025-11-13 23:10:11 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+//Programming exercise//
+#include<stdio.h>
+void main()
+{
+    int N1, N2;
+    printf("enter first number N1:");
+    scanf("%d",&N1);
+    
+    printf("enter second number N2:");
+    scanf("%d",&N2);
+    
+    int sum= N1 + N2;
+    printf("sum is %d",sum);
+    
+    int substraction= N2 - N2;
+    printf("substraction is %d",substraction);
+    
+    int multiplication= N1* N2;
+    printf("multiplication is %d",multiplication);
+    
+    int division= N1% N2;
+    printf("division is %d",division);
+    
+    
+    
+}
