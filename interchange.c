95d2458a417920c58982b71cb79084c043782127
Rudra24-max <rudra.nile@indiraicem.ac.in>
2025-11-14 01:14:49 +0530

    Add files via upload

---
+++ interchange.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+//Programming exercise//
+#include<stdio.h>
+
+int main()
+{
+    int  a, b, temp;
+    
+    printf("\nenter the first");
+    scanf("%d",&a);
+    printf("\nenter the second");
+    scanf("%d,&b");
+    
+    temp=a;
+    a=b;
+    b=temp;
+    
+    printf("\nAfter interchange:\n");
+    printf("First number: %d\n",a);
+    printf("Second number: %d\n",b);
+    
+    
+    
+}
